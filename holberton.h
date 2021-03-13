38e37037fed5e15aaf20ffd3ca052e4e2bbda3f2
mutantEris <mmires@live.com>
2021-03-13 23:22:37 +0000

    entered remaining prototypes

--- holberton.h
+++ holberton.h
@@ -2,5 +2,10 @@
 #define HOLBERTON_H
 int _printf(const char *format, ...);
 int _putchar(char c);
+int _count_tokens(char *format);
+int _print(char *format);
+char *_itoa(int n);
+int _strlen(char *str);
+int _is_valid_token(char *format, int current_pos);
 
 #endif
