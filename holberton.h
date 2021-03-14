9bc43b651e49a66db59c63775a5bf45c9e92ff3c
mutantEris <mmires@live.com>
2021-03-14 18:57:59 +0000

    added new prototypes

--- holberton.h
+++ holberton.h
@@ -1,5 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _count_tokens(char *format);
