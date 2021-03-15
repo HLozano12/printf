713dbd88bfb3a519b61c130a3cc8cc684851e609
HLozano12 <HLR8503@gmail.com>
2021-03-15 18:40:25 +0000

    Integer.c

---
+++ print_integer.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_integer - Prototype function
+ * Description: Print integer fed
+ * @tok:
+ * @args:
+ *
+ * Return:
+ */
+int print_integer(token_t tok, va_list args)
+{
+
+}
