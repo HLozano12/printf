e1711983efea7b9d3ff3c25c3a21bbbb11c08b43
HLozano12 <HLR8503@gmail.com>
2021-03-15 18:38:44 +0000

    Binary

---
+++ print_binary.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_binary - Prototype function
+ * Description: Print conversion of binary
+ * @tok:
+ * @args:
+ *
+ * Return:
+ */
+int print_binary(token_t tok, va_list args)
+{
+
+}
