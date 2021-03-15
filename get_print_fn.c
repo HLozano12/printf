dd15b56c4369e31adac7e31ccf8327e5f898fba4
HLozano12 <HLR8503@gmail.com>
2021-03-15 21:40:37 +0000

    Add Percent Func

--- get_print_fn.c
+++ get_print_fn.c
@@ -12,6 +12,7 @@ int (*get_print_fn(token_t tok))(token_t, va_list)
 	static format_t fmt[] = {
 		{'c', print_char},
 		{'s', print_string},
+		{'%', print_percent},
 		{'d', print_integer},
 		{'i', print_integer},
 		{'b', print_binary},
