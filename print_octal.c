8b5b97734e2307340abd2b7d191b54f49f70a514
HLozano12 <HLR8503@gmail.com>
2021-03-15 18:41:02 +0000

    Octal

---
+++ print_octal.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_octal - Prototype function
+ * Description: Print octal of 
+ * @tok:
+ * @args:
+ *
+ * Return:
+ */
+int print_octal(token_t tok, va_list args)
+{
+
+}
