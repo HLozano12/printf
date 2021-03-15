420dacb29474b2de9f4e168d0bc94d01bbd4b5b6
HLozano12 <HLR8503@gmail.com>
2021-03-15 20:19:39 +0000

    String Function

--- print_string.c
+++ print_string.c
@@ -2,12 +2,19 @@
 /**
  * print_string - Prototype function
  * Description: Print string provided
- * @tok:
- * @args:
+ * @tok: voided
+ * @args: list of args passed
  *
- * Return:
+ * Return: length of string printed
  */
 int print_string(token_t tok, va_list args)
 {
+	(void)tok;
+	char *string = va_arg(args, char*);
+	int i;
 
+/* passing through string to obtain length */
+	for (i = 0; string[i] != '\0'; i++)
+		_putchar(string[i]);
+	return (i);
 }
