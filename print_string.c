b141e1a2b32b99e75a855311df7e517ef102f79d
HLozano12 <HLR8503@gmail.com>
2021-03-15 20:32:25 +0000

    Print String

--- print_string.c
+++ print_string.c
@@ -9,10 +9,10 @@
  */
 int print_string(token_t tok, va_list args)
 {
-	(void)tok;
 	char *string = va_arg(args, char*);
 	int i;
 
+	(void)tok;
 /* passing through string to obtain length */
 	for (i = 0; string[i] != '\0'; i++)
 		_putchar(string[i]);
